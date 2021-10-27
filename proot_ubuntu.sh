fee0d92adcc5a249cab72a65447a38f85eae11f0
Jeon JongChan <loki377312@gmail.com>
2021-10-28 03:23:31 +0900

    2차 수정 root 권한

--- proot_ubuntu.sh
+++ proot_ubuntu.sh
@@ -1,7 +1,7 @@
 # copy by https://gxmersam.blogspot.com/. thanks you
 # vnc 설치 및 gui 프로그램과 터미널 설치
 # openbox는 저사양GUI로 코딩이나 웹 서치만 할 경우 유용하다 판단해서 냅둠
-apt install tigervnc-standalone-server openbox xterm -y
+sudo apt install tigervnc-standalone-server openbox xterm -y
 echo "rm ~/.vnc/localhost*; vncserver" > ~/vnc.sh
 # sound 설정
 echo "export PULSE_SERVER=127.0.0.1" > ~/.profile
