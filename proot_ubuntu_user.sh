1f2fe6f8d948a108098cc15d075571a5cdad25fd
Jeon JongChan <loki377312@gmail.com>
2021-10-28 03:52:06 +0900

    2차 오타수정

--- proot_ubuntu_user.sh
+++ proot_ubuntu_user.sh
@@ -1,5 +1,5 @@
 # copy by https://gxmersam.blogspot.com/. thanks you
 apt update -y && apt install sudo -y
 adduser tab
-echo "sammy ALL=(ALL:ALL) ALL" > /etc/sudoers
+echo "tab ALL=(ALL:ALL) ALL" > /etc/sudoers
 su - tab
