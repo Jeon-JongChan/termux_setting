3c18c91cbc40927726d37c5d651307fbbc1adcb9
Jeon JongChan <loki377312@gmail.com>
2021-10-28 05:06:01 +0900

    3차 수정 1차

--- proot_switch.sh
+++ proot_switch.sh
@@ -1,4 +1,3 @@
-pkg update -y && pkg install wget -y
 wget https://raw.githubusercontent.com/WMCB-Tech/termux-prefix-switcher/master/termux-prefix-switcher && chmod +x termux-prefix-switcher
 wget https://raw.githubusercontent.com/Jeon-JongChan/termux_setting/master/proot_distro.sh && chmod +x proot_distro.sh
-termux-prefix-switcher switch
+./termux-prefix-switcher switch
