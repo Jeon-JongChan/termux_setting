bd71874c1e8e0245fdfac694d2dd4a1ecc570fe5
Jeon JongChan <loki377312@gmail.com>
2021-10-28 05:43:46 +0900

    3차수정 - wine 설치추가

--- proot_ubuntu.sh
+++ proot_ubuntu.sh
@@ -4,8 +4,8 @@
 sudo apt install tigervnc-standalone-server openbox xterm -y
 echo "rm ~/.vnc/localhost*; vncserver" > ~/vnc.sh
 # sound 설정
-echo "export PULSE_SERVER=127.0.0.1" > ~/.profile
-# wine 설치
+echo "export PULSE_SERVER=127.0.0.1" >> ~/.profile
+# box86 설치
 sudo apt-get install cmake git python3 build-essential gcc -y
 git clone https://github.com/ptitSeb/box86
 cd box86
@@ -13,6 +13,16 @@ mkdir build; cd build;
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make 
 sudo make install
+#box86에 wine설치
+cd ~;
+mkdir ~/wine
+wget https://www.playonlinux.com/wine/binaries/phoenicis/upstream-linux-x86/PlayOnLinux-wine-5.19-upstream-linux-x86.tar.gz;
+tar -xf PlayOnLinux-wine-5.19-upstream-linux-x86.tar.gz -C ~/wine/
+
+echo "export BOX86_PATH=~/wine/bin/" >> ~/.profile
+echo "export BOX86_LD_LIBRARY_PATH=~/wine/lib/" >> ~/.profile
+echo "export BOX86_LOG=1" >> ~/.profile
+echo 'alias wboot="box86 wine"' >> ~/.profile
 
 #기타 프로그램 설치
 # 크로미움
