f72e1bf531d3592ec7d5b5a5617c63787eba69fd
Jeon JongChan <loki377312@gmail.com>
2021-10-28 05:12:40 +0900

    3차 수정 - 2차 우분투 유저파일에 우분투 파일 다운로드 추가

--- proot_ubuntu_user.sh
+++ proot_ubuntu_user.sh
@@ -2,4 +2,10 @@
 apt update -y && apt install sudo -y
 adduser tab
 echo "tab ALL=(ALL:ALL) ALL" > /etc/sudoers
+# 미리 설치할 파일 다운로드
+cd /home; mkdir tab; cd tab
+wget https://raw.githubusercontent.com/Jeon-JongChan/termux_setting/master/proot_ubuntu.sh
+chown tab:tab proot_ubuntu.sh
+chmod 755 proot_ubuntu.sh
+
 su - tab
