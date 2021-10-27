61b65cac6ca93765a05418b513d65fc7a6b56f28
Jeon JongChan <loki377312@gmail.com>
2021-10-28 02:51:34 +0900

    1차 수정 - proot_distro

--- proot_distro.sh
+++ proot_distro.sh
@@ -1,16 +1,16 @@
 # copy by https://gxmersam.blogspot.com thanks you
-root=/data/data/com/termux/files
+root=/data/data/com.termux/files
 # 64bit > 32bit(x86) ( wine 설치할려면 필요 )
-pkg update && pkg install proot-distro pulseaudio wget -y
+pkg update -y && pkg install proot-distro pulseaudio wget -y
 wget https://raw.githubusercontent.com/WMCB-Tech/termux-prefix-switcher/master/termux-prefix-switcher
 termux-prefix-switcher switch
 # ****termux ubuntu 설치 및 설정 시작
-pkg update && pkg upgrade && pkg install proot-distro pulseaudio -y
+pkg update -y && pkg upgrade && pkg install proot-distro pulseaudio -y
 # termux sound set
 pulseaudio --start --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 
 # ubuntu alias 사라진듯
+echo "alias uboot=proot-distro login --user tab ubuntu" >> ${root}/usr/etc/bash.bashrc
 proot-distro install ubuntu
-echo "alias uboot=proot-distro login --user tab ubuntu" > ${root}/usr/etc/bash.bashrc
 proot-distro login ubuntu
