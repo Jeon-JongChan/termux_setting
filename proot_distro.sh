9e3be549af5f67c56cc511a1c0ccc8bf66a8ef63
Jeon JongChan <loki377312@gmail.com>
2021-10-28 04:37:05 +0900

    2차 버그 수정

--- proot_distro.sh
+++ proot_distro.sh
@@ -2,7 +2,7 @@
 root=/data/data/com.termux/files
 # 64bit > 32bit(x86) ( wine 설치할려면 필요 )
 pkg update -y && pkg install proot-distro pulseaudio wget -y
-wget https://raw.githubusercontent.com/WMCB-Tech/termux-prefix-switcher/master/termux-prefix-switcher
+wget https://raw.githubusercontent.com/WMCB-Tech/termux-prefix-switcher/master/termux-prefix-switcher && chmod +x termux-prefix-switcher
 termux-prefix-switcher switch
 # ****termux ubuntu 설치 및 설정 시작
 pkg update -y && pkg upgrade && pkg install proot-distro pulseaudio -y
