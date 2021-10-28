7feaf7092c7e2b2c29e68310a25caf8e65151a35
Jeon JongChan <loki377312@gmail.com>
2021-10-29 03:49:08 +0900

    4차 - uroot 경로수정

--- install/install_tool.sh
+++ install/install_tool.sh
@@ -1,11 +1,13 @@
 echo "termux ubuntu program installer"
-echo "1. vscode"
-echo "2. cromium"
+echo "1. install cromium"
+echo "2. remove cromium"
+echo "3. install vscode"
+echo "4. remove vscode"
 echo -e "select number : "
 read program
 
 if [${program} -eq 1]; then
-elif p[${program} -eq 1]; then
+elif [${program} -eq 1]; then
 fi
 
 function vscode() {
