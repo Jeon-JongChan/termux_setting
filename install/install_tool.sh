556e3ad023d3a60285ce4f051124253647682902
Jeon JongChan <loki377312@gmail.com>
2021-10-30 03:09:32 +0900

    5차 - 함수 위치변경

--- install/install_tool.sh
+++ install/install_tool.sh
@@ -1,34 +1,3 @@
-echo "termux ubuntu program installer."
-echo "double number is remove(ex. 1 install 11 remove)"
-echo "1. install cromium"
-echo "2. install vscode"
-echo "3. install ibus"
-echo "4. install grapejuice(roblox studio)"
-echo "5. install box86"
-echo "6. install box86_64"
-
-echo -n "select number : "
-read program
-echo ""
-
-if [ ${program} -eq 1 ]; then
-    echo "********** install cromium **********"
-    cromium
-elif [ ${program} -eq 2 ]; then
-    vscode
-elif [ ${program} -eq 3 ]; then
-    ibus
-elif [ ${program} -eq 4 ]; then
-    echo "********** install roblox studio **********"
-    roblox
-elif [ ${program} -eq 5 ]; then
-    box86
-elif [ ${program} -eq 6 ]; then
-    box86_64
-elif [ ${program} -eq 55 ]; then
-    remove_box86
-fi
-
 function vscode() {
     sudo apt install -y git python gcc g++ make libx11-dev libxkbfile-dev fakeroot rpm libsecret-1-dev
     sudo apt install -y nodejs npm
@@ -129,3 +98,34 @@ function remove_box86() {
 function ibus() {
     sudo apt install -y fonts-unfonts-core fonts-nanum ibus-hangul
 }
+echo "termux ubuntu program installer."
+echo "double number is remove(ex. 1 install 11 remove)"
+echo "1. install cromium"
+echo "2. install vscode"
+echo "3. install ibus"
+echo "4. install grapejuice(roblox studio)"
+echo "5. install box86"
+echo "6. install box86_64"
+
+echo -n "select number : "
+read program
+echo ""
+
+if [ ${program} -eq 1 ]; then
+    echo "********** install cromium **********"
+    cromium
+elif [ ${program} -eq 2 ]; then
+    vscode
+elif [ ${program} -eq 3 ]; then
+    ibus
+elif [ ${program} -eq 4 ]; then
+    echo "********** install roblox studio **********"
+    roblox
+elif [ ${program} -eq 5 ]; then
+    box86
+elif [ ${program} -eq 6 ]; then
+    box86_64
+elif [ ${program} -eq 55 ]; then
+    remove_box86
+fi
+
